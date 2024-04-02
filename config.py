ca9d9455dac56414547011805b4d57af4afb793c
traviicii <125103256+traviicii@users.noreply.github.com>
2024-04-02 15:15:53 -0400

    updated config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
